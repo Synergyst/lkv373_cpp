841eb3f78c46783c8b25d6b1b6c2146334238835
Synergyst <administrator@mail.synergyst.club>
2025-09-10 04:34:22 -0500

    Start implementing KVM-Proxy protocol support

--- lkv373_check.sh
+++ lkv373_check.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(timeout 0.5 ./lkv373_sniffer --mkv=0 --debug=1 ; exit 0) | ffprobe -hide_banner -i pipe:0
+(timeout 0.5 lkv373_sniffer --mkv=0 --debug=0 ; exit 0) | ffprobe -hide_banner -i pipe:0 2>&1 | grep 'Video:' | awk '{ printf "Format: %s%s Resolution: %s, FPS: %d\n", $6, $7, $8, $13 }'
--- lkv373_sniffer_build.sh
+++ lkv373_sniffer_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #nano lkv373_sniffer.cpp
-g++ -std=c++17 -O2 -Wall -Wextra -pthread -o /usr/local/bin/lkv373_sniffer lkv373_sniffer.cpp -lpcap
+g++ -std=c++17 -O2 -Wall -Wextra -pthread -o /usr/local/bin/lkv373_sniffer lkv373_sniffer.cpp -lpcap -ljpeg
